4a9c6518f3960bdf5a0bcfe1fd0c85880262390b
Brandon Yee <6111102+Firebird1029@users.noreply.github.com>
2022-01-06 12:27:56 -1000

    add progressive image loading

--- src/Feed.jsx
+++ src/Feed.jsx
@@ -1,6 +1,7 @@
 import PropTypes from "prop-types";
 import { Layout, Card, Button, TextContainer, Heading, TextStyle } from "@shopify/polaris";
 import { ThumbsUpMajor } from "@shopify/polaris-icons";
+import ProgressiveImage from "react-progressive-image";
 
 function Feed({ nasaItems }) {
 	return (
@@ -8,11 +9,27 @@ function Feed({ nasaItems }) {
 			{nasaItems.map(
 				({ copyright, date, explanation, hdurl, media_type: mediaType, title, url }) =>
 					mediaType === "image" && (
-						<Layout.Section oneHalf>
+						<Layout.Section oneHalf key={url}>
 							<Card sectioned title={title}>
 								<TextContainer>
 									{/* Image */}
-									<img src={hdurl} alt={title} name={title} style={{ width: "100%" }} />
+									<ProgressiveImage src={hdurl} placeholder={url}>
+										{(src, loading) => (
+											<div style={{ minHeight: loading ? "30rem" : 0 }}>
+												<img
+													style={{
+														width: "100%",
+														transition: "opacity ease-in 300ms",
+														opacity: loading ? 0.5 : 1,
+														filter: loading ? "blur(5px)" : "none",
+													}}
+													src={src}
+													alt={title}
+													name={title}
+												/>
+											</div>
+										)}
+									</ProgressiveImage>
 
 									{/* Like Button */}
 									<Button primary icon={ThumbsUpMajor}>
