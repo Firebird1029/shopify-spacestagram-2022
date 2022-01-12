c6a02942bab15d4623c714c45968689ff02ec48d
Brandon Yee <6111102+Firebird1029@users.noreply.github.com>
2022-01-11 20:23:45 -1000

    add key features description

--- src/App.jsx
+++ src/App.jsx
@@ -37,12 +37,40 @@ function App() {
 				<br />
 				<DisplayText size="extraLarge">Spacestagram</DisplayText>
 				<br />
-				<Card sectioned title="Welcome to Spacestagram!">
+				<Card sectioned title="Welcome to Spacestagram, image-sharing from the final frontier!">
 					<TextContainer>
 						<p>
-							This modest website pulls images from NASA&apos;s APOD (Astronomy Picture of the Day) API
-							and allows you to like/unlike your favorites.
+							This website pulls images from NASA&apos;s APOD (Astronomy Picture of the Day) API and
+							allows you to like/unlike your favorites. The website design comes from{" "}
+							<a
+								href="https://polaris.shopify.com/"
+								style={{ color: "white" }}
+								target="_blank"
+								rel="noreferrer"
+							>
+								Shopify&apos;s Polaris library
+							</a>
+							.
 						</p>
+						<p>
+							Use the date picker below (powered by the Polaris design library) to view images from a
+							specific range of dates.
+						</p>
+						<p>
+							<strong>Key Features:</strong>
+						</p>
+						<ul>
+							<li>
+								The images are progressively loaded from NASA&apos;s API. First, the thumbnail of the
+								image (provided from NASA) is shown and intentionally blurred. Once the HD image is
+								loaded, it is swapped in and the image is unblurred.
+							</li>
+							<li>This website uses LocalStorage to keep track of an image&apos;s like status.</li>
+							<li>
+								Each image post is its own React component, so changing its state (liking/unliking the
+								image) will only re-render that specific image and not the whole page.
+							</li>
+						</ul>
 					</TextContainer>
 				</Card>
 				<br />
